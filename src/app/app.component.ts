81ce98f84dd168e589dec31be196cd057a645f87
COLLOT <gregory.collot@open-groupe.com>
2018-10-23 17:29:07 +0200

    Add app loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
+  // tslint:disable-next-line:component-selector
   selector: 'App',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
- 
-}
+export class AppComponent {}
